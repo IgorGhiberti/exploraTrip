d8ec76cbe18704be2879085f0e27dc9d4f9d6fb5
IgorGhiberti <igorgh@outlook.com>
2025-08-21 20:15:54 -0300

    Implementando os primeiros testes unitários

---
+++ Tests/Entities/UserTests.cs
@@ -0,0 +1,55 @@
+using Xunit;
+using Domain.User;
+using Bogus;
+
+namespace Tests.Entities
+{
+    public sealed class UserTests
+    {
+        private readonly Faker _faker = new("pt_BR");
+        public UserTests(Faker faker)
+        {
+            _faker = faker;
+        }
+        [Fact]
+        public void Constructor_GivenAllParameters_ThenShouldSetThePropertiesCorrectly()
+        {
+            //Arrange
+            var expectedEmail = _faker.Person.Email;
+            var expectedUserName = _faker.Person.FullName;
+            var expectedHashPassword = _faker.Random.AlphaNumeric(20);
+            var expectedActive = true;
+
+            var user = new User(expectedEmail, expectedUserName, expectedHashPassword, expectedActive);
+
+            //Assert
+            Assert.Equal(expectedEmail, user.Email!.Value);
+            Assert.Equal(expectedHashPassword, user.HashPassword);
+            Assert.Equal(expectedUserName, user.UserName);
+            Assert.Equal(expectedActive, user.Active);
+        }
+
+        [Fact]
+        public void UpdateUserMethod_GivenSomeParameter_ThenShouldSetThePropertieCorrectly()
+        {
+            string? expectedEmail = _faker.Person.Email.OrNull(_faker, 0.3f);
+            string? expectedUserName = _faker.Person.FullName.OrNull(_faker, 0.1f);
+            string? expectedHashPassword = _faker.Random.AlphaNumeric(20).OrNull(_faker, 0.39f);
+
+            var user = new User("igorgh@gmail.com", "Igor", "12345");
+            user.UpdateUser(expectedEmail, expectedUserName, expectedHashPassword);
+
+            if (!string.IsNullOrWhiteSpace(expectedEmail))
+                Assert.Equal(expectedEmail, user.Email!.Value);
+            Assert.Equal(user.Email!.Value, user.Email!.Value);
+
+            if (!string.IsNullOrWhiteSpace(expectedUserName))
+                Assert.Equal(expectedUserName, user.UserName);
+            Assert.Equal(user.UserName, user.UserName);
+
+            if (!string.IsNullOrWhiteSpace(expectedHashPassword))
+                Assert.Equal(expectedHashPassword, user.HashPassword);
+            Assert.Equal(user.HashPassword, user.HashPassword);
+        }
+    }
+}
